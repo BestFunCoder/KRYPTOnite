a265fea326edbd1ee48c291e294756ff0ffe17b2
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-07 17:36:27 +0300

    add Main.java class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // вітання
+        new Welcom().welcome();
+        //запуск проги
+        new Menu.run();
+    }
+}
