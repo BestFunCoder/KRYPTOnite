660dff2ddc0eb403c8c323a61d045a9223705475
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-07 20:15:51 +0300

    class creation Alphabet.java

---
+++ src/Alphabet.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Alphabet {
+    private static final Character[] ALPHABET = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    public List<Character> alphabet = new ArrayList<>(Arrays.asList(ALPHABET));
+
+    private static final Character[] PUNCTUATION = {'.', ',', '"', ':', '!', '?', ' ', ';'};
+    public List<Character> punctuation = new ArrayList<>(Arrays.asList(PUNCTUATION));
+
+    public List<Character> getPunctuation() {
+        return punctuation;
+    }
+
+    public List<Character> alphabetWithPunctuation = new ArrayList<>();
+
+    public List<Character> getAlphabetWithPunctuation() {
+        alphabetWithPunctuation.addAll(alphabet);
+        alphabetWithPunctuation.addAll(punctuation);
+        return alphabetWithPunctuation;
+    }
+
+    public int getLengthAWP() {
+        return alphabetWithPunctuation.size();
+    }
+}
