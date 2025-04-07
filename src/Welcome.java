99ac04866358fcbc3a3dc9db6b2b9d5ee319bbf4
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-07 21:20:00 +0300

    class creation Welcome.java

---
+++ src/Welcome.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Welcome {
+    public static final String WELCOME = """
+            === Welcome to the cryptanalyzer program \"Caesar\"! ===
+            
+            """;
+
+    public void welcome() {
+        System.out.println(WELCOME);
+    }
+}
