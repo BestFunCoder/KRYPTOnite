2bbd49a26284f82fb457c8fb218e6550b556e62b
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-08 08:46:08 +0300

    temp commit Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,19 @@
 package src;
 
 public class Menu {
-    public void start(){
+
+    private int operationSelect;
+
+    public void start(
+            // Вибір операції - шифрування або розшифрування
+            operationSelect = new Operation().operationSelection();
+        switch (operationSelect) {
+        case 1 -> encryptRun();
+        case 2 -> decryptRun();
+        case 3 -> starting();
+        case 4 -> System.exit(0);
+    }
+    ){
 
     }
 }
