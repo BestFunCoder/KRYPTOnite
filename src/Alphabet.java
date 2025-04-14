2c3b5dc94decce1673af1d4cd6030c93bfe18727
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-14 07:35:50 +0300

    fix class Alphabet

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -2,22 +2,21 @@ package src;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Alphabet {
     private static final Character[] ALPHABET = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    public List<Character> alphabet = new ArrayList<>(Arrays.asList(ALPHABET));
+    public ArrayList<Character> alphabet = new ArrayList<>(Arrays.asList(ALPHABET));
 
     private static final Character[] PUNCTUATION = {'.', ',', '"', ':', '!', '?', ' ', ';'};
-    public List<Character> punctuation = new ArrayList<>(Arrays.asList(PUNCTUATION));
+    public ArrayList<Character> punctuation = new ArrayList<>(Arrays.asList(PUNCTUATION));
 
-    public List<Character> getPunctuation() {
+    public ArrayList<Character> getPunctuation() {
         return punctuation;
     }
 
-    public List<Character> alphabetWithPunctuation = new ArrayList<>();
+    public ArrayList<Character> alphabetWithPunctuation = new ArrayList<>();
 
-    public List<Character> getAlphabetWithPunctuation() {
+    public ArrayList<Character> getAlphabetWithPunctuation() {
         alphabetWithPunctuation.addAll(alphabet);
         alphabetWithPunctuation.addAll(punctuation);
         return alphabetWithPunctuation;
